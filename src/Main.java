9d5a96df945956fac16110726060bb3ea6b1c368
CrescentMnn <coeus.ti@outlook.com>
2024-05-25 01:08:07 -0600

    Update Main.java
    Deleted unnecessary comments for debug

--- src/Main.java
+++ src/Main.java
@@ -12,52 +12,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new Myframe();
-		
-		/*redSlider = new JSlider(0,255,0);
-		redSlider.setPreferredSize(new Dimension(70,200));
-		redSlider.setPaintTicks(true);
-		redSlider.setMinorTickSpacing(25);
-		
-		redSlider.setPaintTrack(true);
-		redSlider.setMajorTickSpacing(50);
-		
-		redSlider.setPaintLabels(true);
-		redSlider.setBackground(Color.white);
-		
-		redSlider.setOrientation(SwingConstants.VERTICAL);
-		redSlider.setOpaque(false);
-		redSlider.addChangeListener((ChangeListener) this);
 	
-		
-		greenSlider = new JSlider(0,255,0);
-		greenSlider.setPreferredSize(new Dimension(70,200));
-		greenSlider.setPaintTicks(true);
-		greenSlider.setMinorTickSpacing(25);
-		
-		greenSlider.setPaintTrack(true);
-		greenSlider.setMajorTickSpacing(50);
-		
-		greenSlider.setPaintLabels(true);
-		greenSlider.setBackground(Color.white);
-		
-		greenSlider.setOrientation(SwingConstants.VERTICAL);
-		greenSlider.setOpaque(false);
-		greenSlider.addChangeListener((ChangeListener) this);
-		
-		blueSlider = new JSlider(0,255,0);
-		blueSlider.setPreferredSize(new Dimension(70,200));
-		blueSlider.setPaintTicks(true);
-		blueSlider.setMinorTickSpacing(25);
-		
-		blueSlider.setPaintTrack(true);
-		blueSlider.setMajorTickSpacing(50);
-		
-		blueSlider.setPaintLabels(true);
-		blueSlider.setBackground(Color.white);
-		blueSlider.setOpaque(false);
-		
-		blueSlider.setOrientation(SwingConstants.VERTICAL);
-		blueSlider.addChangeListener((ChangeListener) this);*/
 	}
 
 }
